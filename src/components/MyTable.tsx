6941d400601ee27d9b54e937a32e7658afbda79a
sharifabee <sharifabee.alur@gmail.com>
2025-04-18 11:31:52 +0530

    photos added

--- src/components/MyTable.tsx
+++ src/components/MyTable.tsx
@@ -1,44 +1,104 @@
-import React from 'react'
+import React from "react";
 
 export default function MyTable() {
+  const photos = [
+    {
+      albumId: 1,
+      id: 1,
+      title: "accusamus beatae ad facilis cum similique qui sunt",
+      url: "https://via.placeholder.com/600/92c952",
+      thumbnailUrl: "https://via.placeholder.com/150/92c952",
+    },
+    {
+      albumId: 1,
+      id: 2,
+      title: "reprehenderit est deserunt velit ipsam",
+      url: "https://via.placeholder.com/600/771796",
+      thumbnailUrl: "https://via.placeholder.com/150/771796",
+    },
+    {
+      albumId: 1,
+      id: 3,
+      title: "officia porro iure quia iusto qui ipsa ut modi",
+      url: "https://via.placeholder.com/600/24f355",
+      thumbnailUrl: "https://via.placeholder.com/150/24f355",
+    },
+    {
+      albumId: 1,
+      id: 4,
+      title: "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+      url: "https://via.placeholder.com/600/d32776",
+      thumbnailUrl: "https://via.placeholder.com/150/d32776",
+    },
+    {
+      albumId: 1,
+      id: 5,
+      title: "natus nisi omnis corporis facere molestiae rerum in",
+      url: "https://via.placeholder.com/600/f66b97",
+      thumbnailUrl: "https://via.placeholder.com/150/f66b97",
+    },
+    {
+      albumId: 1,
+      id: 6,
+      title: "accusamus ea aliquid et amet sequi nemo",
+      url: "https://via.placeholder.com/600/56a8c2",
+      thumbnailUrl: "https://via.placeholder.com/150/56a8c2",
+    },
+    {
+      albumId: 1,
+      id: 7,
+      title: "officia delectus consequatur vero aut veniam explicabo molestias",
+      url: "https://via.placeholder.com/600/b0f7cc",
+      thumbnailUrl: "https://via.placeholder.com/150/b0f7cc",
+    },
+    {
+      albumId: 1,
+      id: 8,
+      title: "aut porro officiis laborum odit ea laudantium corporis",
+      url: "https://via.placeholder.com/600/54176f",
+      thumbnailUrl: "https://via.placeholder.com/150/54176f",
+    },
+    {
+      albumId: 1,
+      id: 9,
+      title: "qui eius qui autem sed",
+      url: "https://via.placeholder.com/600/51aa97",
+      thumbnailUrl: "https://via.placeholder.com/150/51aa97",
+    },
+  ];
+
   return (
     <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Album</th>
+            <th scope="col">Title</th>
+            <th scope="col">Url</th>
+          </tr>
+        </thead>
+        <tbody>
 
+{photos.map(
 
-<table className="table">
-  <thead>
+(eachPhoto)=>(
+
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>John</td>
-      <td>Doe</td>
-      <td>@social</td>
-    </tr>
-  </tbody>
-</table>
+    <th scope="row">{eachPhoto.id}</th>
+    <td>{eachPhoto.albumId}</td>
+    <td>{eachPhoto.title}</td>
+    <td>{eachPhoto.url}</td>
+  </tr>
+  
+)
 
 
+)}
 
-
+         
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
