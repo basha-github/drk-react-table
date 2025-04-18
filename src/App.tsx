232c9c3f676fcd948bee2c57df41284137a98217
sharifabee <sharifabee.alur@gmail.com>
2025-04-18 11:18:29 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import TableComponent from './components/TableComponent'
+import UsersTable from './components/UsersTable'
+import MoviesTable from './components/MoviesTable'
+import MyTable from './components/MyTable'
+
+export default function App() {
+  return (
+    <div>
+
+<MyTable />
+
+
+
+    </div>
+  )
+}
